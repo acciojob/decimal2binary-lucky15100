f80887ce05a738ec3362bd2ff10697f1fb2e503c
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-07-28 15:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimalNum) {
+  let binaryNum = "";
+
+  if (decimalNum === 0) {
+    binaryNum = "0";
+  } else {
+    while (decimalNum > 0) {
+      let remainder = decimalNum % 2;
+      binaryNum = remainder + binaryNum;
+      decimalNum = Math.floor(decimalNum / 2);
+    }
+  }
+
+  return binaryNum;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Test cases
+const inputNum1 = 7;
+const inputNum2 = 10;
+const inputNum3 = 33;
+
+console.log("Input:", inputNum1);
+console.log("Output:", decimalToBinary(inputNum1));
+
+console.log("Input:", inputNum2);
+console.log("Output:", decimalToBinary(inputNum2));
+
+console.log("Input:", inputNum3);
+console.log("Output:", decimalToBinary(inputNum3));
